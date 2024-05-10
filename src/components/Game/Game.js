22173216b6624b5953a5d8fc12c76aee80adf257
Chris Stevenson <chrislstevenson@gmail.com>
2024-05-10 12:16:34 -0400

    Add input for Wordle guesses
    - this is my attempt without watching the solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -9,9 +9,30 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guess, setGuess] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(guess);
+    setGuess('');
+  }
+
   return (
     <>
-    </>;
+      <form
+        className="guess-input-wrapper"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          pattern="[A-Z]{5}"
+          required
+          value={guess}
+          onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+    </>
   )
 }
 
