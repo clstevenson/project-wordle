4c80a8f33c2e3311e241cb613638d8c9d6c370f6
Chris Stevenson <chrislstevenson@gmail.com>
2024-05-10 12:37:24 -0400

    Refactor to use component
    - still without looking at solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
+import GuessInput from '../GuessInput';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
@@ -10,29 +12,9 @@ console.info({ answer });
 
 function Game() {
   const [guess, setGuess] = useState('');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(guess);
-    setGuess('');
-  }
 
   return (
-    <>
-      <form
-        className="guess-input-wrapper"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="guess-input">Enter guess:</label>
-        <input
-          id="guess-input"
-          type="text"
-          pattern="[A-Z]{5}"
-          required
-          value={guess}
-          onChange={(e) => setGuess(e.target.value.toUpperCase())}
-        />
-      </form>
-    </>
+    <Guess Input guess={guess} setGuess={setGuess}/>
   )
 }
 
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function GuessInput({ guess, setGuess }) {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(guess);
+    setGuess('');
+  };
+
+  return (
+        <form
+        className="guess-input-wrapper"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          pattern="[A-Z]{5}"
+          required
+          value={guess}
+          onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+  )
+}
+
+export default GuessInput;
