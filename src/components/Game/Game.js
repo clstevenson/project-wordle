f8e6d91c70f079a51134942603a8869dce05d843
Chris Stevenson <chrislstevenson@gmail.com>
2024-05-11 06:39:09 -0400

    End the game with message
    - implemented before looking at solution vid

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
+import GameOver from '../GameOver';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -13,9 +15,18 @@ console.info({ answer });
 
 function Game() {
   const [guessResults, setGuessResults] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
 
   const handleSubmitGuess = (newGuess) => {
     const guessArray = [...guessResults, newGuess];
+    if (newGuess===answer) {
+      setGameOver(true);
+      setGameWon(true);
+    } else if (guessArray.length === NUM_OF_GUESSES_ALLOWED) {
+      setGameOver(true);
+      setGameWon(false);
+    }
     setGuessResults(guessArray);
   };
 
@@ -23,6 +34,7 @@ function Game() {
     <>
       <GuessResults guessResults={guessResults} answer={answer}/>
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
+    {gameOver && <GameOver gameWon={gameWon} answer={answer} numGuesses={guessResults.length}/>}
     </>
   );
 }
